1eea5998c8f30ce206c167d828da3f2cc3a4c46a
lvnvn177 <53424393+lvnvn177@users.noreply.github.com>
2024-11-11 05:00:17 +0900

    version3

--- test.py
+++ test.py
@@ -5,7 +5,6 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 FILE_PATH = os.path.join(BASE_DIR, '2304.01852v4_22.pdf')
 url = "http://127.0.0.1:2000/s3r/upload"  # FastAPI 서버 URL
 
-file_path = "/Users/iyeongho/Desktop/converter_api/2304.01852v4_22.pdf"
 with open(FILE_PATH, "rb") as file:
     response = requests.post(url, files={"file": file})
 
